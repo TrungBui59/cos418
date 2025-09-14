8861f64a72ab0185d219c80723f7d1d10ecc182e
TrungBui59 <trungbui.work2022@gmail.com>
2025-09-13 18:57:46 -0700

    Finished question 2 of lab1
    Signed-off-by: TrungBui59 <trungbui.work2022@gmail.com>

--- assignment1-1/q2.go
+++ assignment1-1/q2.go
@@ -3,15 +3,23 @@ package cos418_hw1_1
 import (
 	"bufio"
 	"io"
+	"os"
 	"strconv"
+	"sync"
 )
 
 // Sum numbers from channel `nums` and output sum to `out`.
 // You should only output to `out` once.
 // Do NOT modify function signature.
-func sumWorker(nums chan int, out chan int) {
-	// TODO: implement me
-	// HINT: use for loop over `nums`
+func sumWorker(wg *sync.WaitGroup, nums chan int, out chan int) {
+	defer wg.Done()
+	res := 0
+
+	for val := range nums {
+		res += val
+	}
+
+	out <- res
 }
 
 // Read integers from the file `fileName` and return sum of all values.
@@ -23,10 +31,48 @@ func sum(num int, fileName string) int {
 	// TODO: implement me
 	// HINT: use `readInts` and `sumWorkers`
 	// HINT: used buffered channels for splitting numbers between workers
-	return 0
+	file, err := os.Open(fileName)
+	checkError(err)
+	defer file.Close()
+
+	nums, err := readInts(file)
+	checkError(err)
+
+	channels := make([]chan int, num)
+	for i := range channels {
+		channels[i] = make(chan int, len(nums)/num+1)
+	}
+
+	out := make(chan int, num)
+	var wg sync.WaitGroup
+	wg.Add(num)
+
+	for i := 0; i < num; i++ {
+		go sumWorker(&wg, channels[i], out)
+	}
+
+	for i, number := range nums {
+		channels[i%num] <- number
+	}
+
+	for i := range channels {
+		close(channels[i])
+	}
+
+	wg.Wait()
+	close(out)
+
+	totalSum := 0
+	for partialSum := range out {
+		totalSum += partialSum
+	}
+
+	return totalSum
 }
 
-// Read a list of integers separated by whitespace from `r`.
+
+
+// Read a list of integers from `r`.
 // Return the integers successfully read with no error, or
 // an empty slice of integers and the error that occurred.
 // Do NOT modify this function.
